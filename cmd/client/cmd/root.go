6d5fd73a7e1ca8792c8ffc86ce07ac9ba09f7948
Elina Pakharevskaya <elina.paxar@gmail.com>
2024-07-11 20:18:24 +0300

    selete useless comments (#6)

--- cmd/client/cmd/root.go
+++ cmd/client/cmd/root.go
@@ -12,7 +12,6 @@ var rootCmd = &cobra.Command{
 	Short: "Set hostname and change the list of dns servers.",
 	Long: `Hostsetup is a utility for Linux that set hostname and
 change (add and delete) the list of DNS servers.`,
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -25,13 +24,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hostsetup-service.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/client/cmd/setHostname.go
+++ cmd/client/cmd/setHostname.go
@@ -11,7 +11,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// setHostnameCmd represents the setHostname command
 var setHostnameCmd = &cobra.Command{
 	Use:   "set-hostname",
 	Short: "Set new hostname",
@@ -46,14 +45,4 @@ the new hostname. If success returns zero, otherwise an error.`,
 
 func init() {
 	rootCmd.AddCommand(setHostnameCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// setHostnameCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// setHostnameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
